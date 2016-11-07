dade6adfdff425287dce9a67e881366f2cdccb6e
Pavel Torgashov <p_torgashov@ukr.net>
2016-11-07 13:30:15 +0200

    Main model classes

--- MainRoadModel/Game.cs
+++ MainRoadModel/Game.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using MainRoadModel.Model;
 
 namespace MainRoadModel
 {
-    public static class Game
+    static class Game
     {
+        public static GameState State { get; set; }
+        /// <summary>
+        /// Size of tile grid
+        /// </summary>
+        public const int GRIDSIZE = 256;
+        /// <summary>
+        /// Size of cell in meters
+        /// </summary>
+        public const float METERS_PER_UNIT = 12.8f;
+
+        public const string DEF_FILENAME = "mainroad.bin";
+
+        public static void Load(string name = DEF_FILENAME)
+        {
+            var file = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + name;
+            if (File.Exists(file))
+                try
+                {
+                    using (var str = File.OpenRead(file))
+                    {
+                        //load saved state
+                        State = (GameState)new BinaryFormatter().Deserialize(str);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Write(ex);
+                }
+        }
+
+        public static void Save(string name = DEF_FILENAME)
+        {
+            var file = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + name;
+            try
+            {
+                using (var str = File.OpenWrite(file))
+                {
+                    new BinaryFormatter().Serialize(str, State);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+        }
     }
 }
---
+++ MainRoadModel/Model/GameState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainRoadModel.Model
+{
+    /// <summary>
+    /// Game state
+    /// </summary>
+    [Serializable]
+    class GameState
+    {
+        /// <summary>
+        /// Grid of tiles
+        /// </summary>
+        public Cell[,] Cells { get; private set; }
+
+        public GameState()
+        {
+            Cells = new Cell[Game.GRIDSIZE, Game.GRIDSIZE];
+        }
+    }
+
+    /// <summary>
+    /// Cell of tile's grid
+    /// </summary>
+    [Serializable]
+    class Cell
+    {
+        public int CellX { get; set; }
+        public int CellY { get; set; }
+        public LinkedList<Tile> Tiles { get; set; }
+    }
+
+    /// <summary>
+    /// Tile
+    /// </summary>
+    [Serializable]
+    class Tile
+    {
+        public Cell ParentCell { get; set; }
+    }
+}
